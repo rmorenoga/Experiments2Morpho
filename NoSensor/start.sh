010d1fff33b5f552684efbeea7c68f81a3a24872
rmorenoga <rmorenoga@unal.edu.co>
2019-09-25 11:30:22 -0400

    First commit

---
+++ NoSensor/start.sh
@@ -0,0 +1,10 @@
+echo "Start" >> timelog.txt
+date >> timelog.txt
+
+java -Djava.library.path=/home/rodrigo/Evolution/ -jar HAEAS.jar 15
+
+
+echo "Finished" >> timelog.txt
+date >> timelog.txt
+
+echo Finished 
