cfbc8892246a1fcb773e8c11fceef7b76918321d
rmorenoga <rmorenoga@unal.edu.co>
2019-10-22 18:37:20 -0400

    Configured CPGSeed experiments

--- CPGSeed/start.sh
+++ CPGSeed/start.sh
@@ -1,7 +1,7 @@
 echo "Start" >> timelog.txt
 date >> timelog.txt
 
-java -Djava.library.path=/home/rodrigo/Evolution/ -jar HAEAS.jar 15
+java -Djava.library.path=/home/rodrigo/Evolution/ -jar HAEAS-2MCPGSeed.jar 15
 
 
 echo "Finished" >> timelog.txt
