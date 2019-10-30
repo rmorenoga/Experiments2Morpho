cf1a27c112a669a95f8e312477312b20ef3dc5c9
rmorenoga <rmorenoga@unal.edu.co>
2019-10-30 08:52:50 -0400

    GenTest Done

--- NoSeed/startGenTest.sh
+++ NoSeed/startGenTest.sh
@@ -1,7 +1,7 @@
 echo "Start" >> timelog.txt
 date >> timelog.txt
 
-java -Djava.library.path=/home/rodrigo/Evolution/ -jar NoSeedGenTest.jar ./ HAEAS-2MNoSeedR GenTestHAEANoSeed.txt HAEANoSeed.csv 2M-HAEA-S 0
+java -Djava.library.path=/home/rodrigo/Evolution/ -jar NoSeedGenTest.jar ./ HAEAS-2MNoSeedR GenTestHAEANoSeed.txt HAEANoSeed.csv 2M-HAEA-S 1
 
 
 echo "Finished" >> timelog.txt
--- NoSensor/startGenTest.sh
+++ NoSensor/startGenTest.sh
@@ -1,7 +1,7 @@
 echo "Start" >> timelog.txt
 date >> timelog.txt
 
-java -Djava.library.path=/home/rodrigo/Evolution/ -jar NoSensorGenTest.jar ./ HAEAS-2MNoSensorR GenTestHAEANoSensor.txt HAEANoSensor.csv 2M-HAEA-NS 0
+java -Djava.library.path=/home/rodrigo/Evolution/ -jar NoSensorGenTest.jar ./ HAEAS-2MNoSensorR GenTestHAEANoSensor.txt HAEANoSensor.csv 2M-HAEA-NS 2
 
 
 echo "Finished" >> timelog.txt
